281c14de2775ae42a68580e78f1a8f5d211fdcc9
rahuljain46 <rahuljain820436@gmail.com>
2021-04-05 20:14:07 +0530

    package remove

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = lazy(() => import("./App"));
 
 render(
